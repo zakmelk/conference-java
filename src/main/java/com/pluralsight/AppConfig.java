3bf8f703f2f4bb15aa6d84fb75986221cd8c382f
zakaria melk <zakariamelk@MacBook-Pro-de-zakaria.local>
2020-03-14 19:46:08 +0100

    Advenced bean config : use FactoryBean for autowiring

--- src/main/java/com/pluralsight/AppConfig.java
+++ src/main/java/com/pluralsight/AppConfig.java
@@ -5,15 +5,30 @@ import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
 import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
+import com.pluralsight.util.CalendarFactory;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import java.util.Calendar;
+
 @Configuration
 @ComponentScan({"com.pluralsight"})
 public class AppConfig {
+
+    @Bean(name = "cal")
+    public CalendarFactory calFactory(){
+        CalendarFactory factory = new CalendarFactory();
+        factory.addDays(2);
+        return factory;
+    }
+
+    @Bean
+    public Calendar call() throws Exception{
+        return calFactory().getObject();
+    }
 
     /*
     @Bean(name = "speakerService")
--- src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateSpeakerRepositoryImpl.java
@@ -1,13 +1,19 @@
 package com.pluralsight.repository;
 
 import com.pluralsight.model.Speaker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 @Repository("speakerRepository")
 public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
+
+    @Autowired
+    private Calendar cal;
+    //this attribute is autowired from the CalendarFactory in appConfig
 
     @Override
     public List<Speaker> findAll(){
@@ -15,6 +21,7 @@ public class HibernateSpeakerRepositoryImpl implements SpeakerRepository {
         Speaker speaker = new Speaker();
         speaker.setFirstName("Zakaria");
         speaker.setLastName("MELK");
+        System.out.println(cal.getTime());
         speakers.add(speaker);
         return speakers;
     }
